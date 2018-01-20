3e780ec57d6ff0786e01209a15480d9b9159c379
Zachary E Buzaid <zbuzaid@gmail.com>
2018-01-19 23:57:21 -0800

    Adding components and folder structure

---
+++ src/app/flight-form/attachments/faa-certificate/faa-certificate.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './faa-certificate.component.html',
+    styleUrls: ['./faa-certificate.component.css']
+})
+export class FAACertificateComponent {
+    title = 'app';
+}
